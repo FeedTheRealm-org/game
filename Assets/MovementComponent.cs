3f0b213d8a7ee738c80758db670362d2111f8788
maxogod <maxi18wars@gmail.com>
2025-10-04 12:29:04 -0300

    feat: collision with walls and ramps

--- Assets/MovementComponent.cs
+++ Assets/MovementComponent.cs
@@ -1,18 +1,18 @@
 using UnityEngine;
 
 public class MovementComponent : MonoBehaviour {
-    [SerializeField] private Transform playerTransform;
+    [SerializeField] private Rigidbody rb;
     [SerializeField] private float moveSpeed = 5f;
     // [SerializeField] private float dashDistance = 2f;
 
-    private Vector2 currentDirection;
+    private Vector3 currentDirection;
 
     public void OnMove(Vector2 direction) {
-        currentDirection = direction;
+        currentDirection = new Vector3(direction.x, 0f, direction.y);
     }
 
-    private void Update() {
-        Vector3 movement = new Vector3(currentDirection.x, 0f, currentDirection.y) * moveSpeed * Time.deltaTime;
-        playerTransform.position += movement;
+    private void FixedUpdate() {
+        Vector3 targetPosition = rb.position + currentDirection * moveSpeed * Time.fixedDeltaTime;
+        rb.MovePosition(targetPosition);
     }
 }
