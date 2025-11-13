22ed55f02280b12311f6dc0c0e4969d529d1314b
maxogod <maxi18wars@gmail.com>
2025-11-13 02:04:42 -0300

    feat: sprite loader support for all parts

--- Assets/1_FeedTheRealm/Core/Systems/SpriteManager/SpriteManager.cs
+++ Assets/1_FeedTheRealm/Core/Systems/SpriteManager/SpriteManager.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System;
+using System.Collections.Generic;
 
 public enum SpritePart {
     None,
@@ -17,34 +18,47 @@ public enum SpritePart {
 
 [CreateAssetMenu(fileName = "SpriteManager", menuName = "Scriptable Objects/SpriteManager")]
 public class SpriteManager : ScriptableObject {
-
     [Header("General Settings")]
     [SerializeField]
     private Logging.Logger logger;
 
+    public Action<string> onHeadChange;
     public Action<string> onHairChange;
+    public Action<string> onFaceHairChange;
+    public Action<string> onEyeBackChange;
+    public Action<string> onClothChange;
+    public Action<string> onArmorChange;
+    public Action<string> onHelmetChange;
+    public Action<string> onFootBootChange;
+    public Action<string> onFootClothChange;
+    public Action<string> onCapeChange;
+
+    private Dictionary<SpritePart, Action<string>> partChangeActions = new Dictionary<SpritePart, Action<string>>();
+
+    public void OnEnable() {
+        partChangeActions[SpritePart.Head] = onHeadChange;
+        partChangeActions[SpritePart.Hair] = onHairChange;
+        partChangeActions[SpritePart.FaceHair] = onFaceHairChange;
+        partChangeActions[SpritePart.EyeBack] = onEyeBackChange;
+        partChangeActions[SpritePart.Cloth] = onClothChange;
+        partChangeActions[SpritePart.Armor] = onArmorChange;
+        partChangeActions[SpritePart.Helmet] = onHelmetChange;
+        partChangeActions[SpritePart.FootBoot] = onFootBootChange;
+        partChangeActions[SpritePart.FootCloth] = onFootClothChange;
+        partChangeActions[SpritePart.Cape] = onCapeChange;
+    }
 
     public void ChangeSprite(SpritePart part, string textureName) {
-        switch (part) {
-            case SpritePart.Hair:
-                onHairChange?.Invoke(textureName);
-                break;
-            default:
-                Debug.LogWarning($"SpriteManager: Unknown SpritePart {part}");
-                break;
-        }
+        partChangeActions[part]?.Invoke(textureName);
     }
 
     public SpritePart GetSpritePartFromCategoryName(string categoryName) {
-        SpritePart part = SpritePart.None;
-        switch (categoryName.ToLower()) {
-            case "hair":
-                part = SpritePart.Hair;
-                break;
-            default:
-                logger.Log($"SpriteManager: Unknown category name {categoryName}", this, Logging.LogType.Warning);
-                break;
+        categoryName = categoryName.Replace(" ", "").Replace("_", "").Replace("-", "");
+        if (Enum.TryParse(categoryName, true, out SpritePart part)) {
+            return part;
         }
-        return part;
+
+        logger.Log($"SpriteManager: Unknown category name {categoryName}", this, Logging.LogType.Warning);
+        return SpritePart.None;
     }
 }
--- Assets/1_FeedTheRealm/Gameplay/Characters/Common/Components/SpriteLoader.cs
+++ Assets/1_FeedTheRealm/Gameplay/Characters/Common/Components/SpriteLoader.cs
@@ -8,24 +8,135 @@ public class SpriteLoader : MonoBehaviour {
     [SerializeField]
     private API.AssetsService assetsService;
 
+    [Header("General settings")]
+    [SerializeField]
+    private Logging.Logger logger;
+
     private void Awake() {
         if (spriteManager != null) {
+            spriteManager.onHeadChange += changeHead;
             spriteManager.onHairChange += changeHair;
+            spriteManager.onFaceHairChange += changeFaceHair;
+            spriteManager.onEyeBackChange += changeEyeBack;
+            spriteManager.onClothChange += changeCloth;
+            spriteManager.onArmorChange += changeArmor;
+            spriteManager.onHelmetChange += changeHelmet;
+            spriteManager.onFootBootChange += changeFootBoot;
+            spriteManager.onFootClothChange += changeFootCloth;
+            spriteManager.onCapeChange += changeCape;
         }
     }
 
-    private void changeHair(string spriteId) {
+    private void OnDestroy() {
+        if (spriteManager != null) {
+            spriteManager.onHeadChange -= changeHead;
+            spriteManager.onHairChange -= changeHair;
+            spriteManager.onFaceHairChange -= changeFaceHair;
+            spriteManager.onEyeBackChange -= changeEyeBack;
+            spriteManager.onClothChange -= changeCloth;
+            spriteManager.onArmorChange -= changeArmor;
+            spriteManager.onHelmetChange -= changeHelmet;
+            spriteManager.onFootBootChange -= changeFootBoot;
+            spriteManager.onFootClothChange -= changeFootCloth;
+            spriteManager.onCapeChange -= changeCape;
+        }
+    }
+
+    /// <summary>
+    /// Replaces the sprite of a given part.
+    /// </summary>
+    private void replacePartSprite(string partName, Sprite newSprite) {
+        GameObject partObject = GameObject.Find(partName);
+        if (partObject != null) {
+            SpriteRenderer spriteRenderer = partObject.GetComponentInChildren<SpriteRenderer>();
+            if (spriteRenderer != null) {
+                spriteRenderer.sprite = newSprite;
+            } else {
+                logger.Log($"SpriteRenderer not found on part: {partName}", this, Logging.LogType.Warning);
+            }
+        } else {
+            logger.Log($"Part GameObject not found: {partName}", this, Logging.LogType.Warning);
+        }
+    }
+
+    /// <summary>
+    /// Downloads a sprite from the AssetsService and creates a sprite.
+    /// </summary>
+    private void downloadSprite(string spriteId, Rect rect, Vector2 pivot, float pxPerUnit, System.Action<Sprite> callback) {
         StartCoroutine(assetsService.DownloadSprite(spriteId, (texture) => {
             if (texture == null) {
+                logger.Log($"Failed to download sprite with ID: {spriteId}", this, Logging.LogType.Error);
                 return;
             }
-            Sprite newHair = Sprite.Create(
+            Sprite newSprite = Sprite.Create(
                 texture,
-                new Rect(0, 0, texture.width, texture.height),
-                new Vector2(0.5f, 0.5f),
-                32f
+                rect,
+                pivot,
+                pxPerUnit
             );
-            GameObject.Find("P_Hair").GetComponentInChildren<SpriteRenderer>().sprite = newHair;
+            callback(newSprite);
         }));
+    }
+
+    /* --- PART CHANGE HANDLERS --- */
+
+    private void changeHead(string spriteId) {
+        downloadSprite(spriteId, new Rect(0, 0, 64, 64), new Vector2(0.5f, 0.5f), 32f, (newHead) => {
+            replacePartSprite("P_Head", newHead);
+        });
+    }
+
+    private void changeHair(string spriteId) {
+        downloadSprite(spriteId, new Rect(0, 0, 64, 64), new Vector2(0.5f, 0.5f), 32f, (newHair) => {
+            replacePartSprite("P_Hair", newHair);
+        });
+    }
+
+    private void changeFaceHair(string spriteId) {
+        downloadSprite(spriteId, new Rect(0, 0, 64, 64), new Vector2(0.5f, 0.5f), 32f, (newFaceHair) => {
+            replacePartSprite("P_FaceHair", newFaceHair);
+        });
+    }
+
+    private void changeEyeBack(string spriteId) {
+        downloadSprite(spriteId, new Rect(0, 0, 64, 64), new Vector2(0.5f, 0.5f), 32f, (newEyeBack) => {
+            replacePartSprite("P_EyeBack", newEyeBack);
+        });
+    }
+
+    private void changeCloth(string spriteId) {
+        downloadSprite(spriteId, new Rect(0, 0, 64, 64), new Vector2(0.5f, 0.5f), 32f, (newCloth) => {
+            replacePartSprite("P_Cloth", newCloth);
+        });
+    }
+
+    private void changeArmor(string spriteId) {
+        downloadSprite(spriteId, new Rect(0, 0, 64, 64), new Vector2(0.5f, 0.5f), 32f, (newArmor) => {
+            replacePartSprite("P_Armor", newArmor);
+        });
+    }
+
+    private void changeHelmet(string spriteId) {
+        downloadSprite(spriteId, new Rect(0, 0, 64, 64), new Vector2(0.5f, 0.5f), 32f, (newHelmet) => {
+            replacePartSprite("P_Helmet", newHelmet);
+        });
+    }
+
+    private void changeFootBoot(string spriteId) {
+        downloadSprite(spriteId, new Rect(0, 0, 64, 64), new Vector2(0.5f, 0.5f), 32f, (newFootBoot) => {
+            replacePartSprite("P_FootBoot", newFootBoot);
+        });
+    }
+
+    private void changeFootCloth(string spriteId) {
+        downloadSprite(spriteId, new Rect(0, 0, 64, 64), new Vector2(0.5f, 0.5f), 32f, (newFootCloth) => {
+            replacePartSprite("P_FootCloth", newFootCloth);
+        });
+    }
+
+    private void changeCape(string spriteId) {
+        downloadSprite(spriteId, new Rect(0, 0, 64, 64), new Vector2(0.5f, 0.5f), 32f, (newCape) => {
+            replacePartSprite("P_Cape", newCape);
+        });
     }
 }
