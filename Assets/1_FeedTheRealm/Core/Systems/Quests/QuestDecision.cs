3f645c75ddf8fb360114b7894bdb78812a89c748
maxogod <maxi18wars@gmail.com>
2026-01-04 18:59:20 -0300

    fix: precommit pass for previous copilot commit

--- Assets/1_FeedTheRealm/Core/Systems/Quests/QuestDecision.cs
+++ Assets/1_FeedTheRealm/Core/Systems/Quests/QuestDecision.cs
@@ -9,13 +9,14 @@ namespace Game.Core.Quests
 
         private bool _isAccepted;
 
-        public QuestData QuestData => _questData;
-
-        public bool IsAccepted => _isAccepted;
         public QuestDecision(QuestData questData, bool isAccepted)
         {
             _questData = questData;
             _isAccepted = isAccepted;
         }
+
+        public QuestData QuestData => _questData;
+
+        public bool IsAccepted => _isAccepted;
     }
 }
