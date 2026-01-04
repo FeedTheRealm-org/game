8d718823fc7ea7605d496ed79f65c5ed14da8fca
maxogod <maxi18wars@gmail.com>
2026-01-04 18:26:01 -0300

    refactor: make NPC interact component keep track of last interactor to notify finish

--- Assets/1_FeedTheRealm/Gameplay/Characters/Npcs/BasicNpc/Scripts/NpcInteractComponent.cs
+++ Assets/1_FeedTheRealm/Gameplay/Characters/Npcs/BasicNpc/Scripts/NpcInteractComponent.cs
@@ -1,21 +1,50 @@
+using Game.Core.Events;
 using Game.Core.Interactions;
+using Game.Core.Quests;
 using UnityEngine;
 
 public class NpcInteractComponent : MonoBehaviour, IInteractable
 {
     [Header("Dialog settings")]
     [SerializeField]
-    private DialogManagerComponent _dialogManager;
+    private DialogManagerComponent dialogManager;
+
+    [Header("Quest settings")]
+    [SerializeField]
+    private QuestDecisionEvent questDecisionEvent;
+
+    [Header("General settings")]
+    [SerializeField]
+    private Logging.Logger logger;
+
+    private IInteractor _currentInteractor;
+
+    private void OnEnable()
+    {
+        questDecisionEvent.OnRaised += OnQuestDecision;
+    }
+
+    private void OnDisable()
+    {
+        questDecisionEvent.OnRaised -= OnQuestDecision;
+    }
 
     public void Interact(IInteractor interactor)
     {
-        _dialogManager.Next();
-        Debug.Log("NPC interacted with by " + interactor.GameObject.name);
-        interactor.FinishInteracting();
+        dialogManager.Next();
+        logger.Log("NPC interacted with by " + interactor.GameObject.name, this);
+        _currentInteractor = interactor;
+        if (!dialogManager.IsQuestOffer())
+            interactor.FinishInteracting();
     }
 
     public bool CanInteract(IInteractor interactor)
     {
         return true;
+    }
+
+    private void OnQuestDecision(QuestDecision _)
+    {
+        _currentInteractor.FinishInteracting();
     }
 }
