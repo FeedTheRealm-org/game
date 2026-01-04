42480b928282281d13db56d26a0d01ca35462b81
maxogod <maxi18wars@gmail.com>
2026-01-04 18:21:19 -0300

    feat: add QuestDecision class

---
+++ Assets/1_FeedTheRealm/Core/Systems/Quests/QuestDecision.cs
@@ -0,0 +1,18 @@
+using System;
+using UnityEngine;
+
+namespace Game.Core.Quests
+{
+    public class QuestDecision
+    {
+        private QuestData _questData;
+
+        private bool _isAccepted;
+
+        public QuestDecision(QuestData questData, bool isAccepted)
+        {
+            _questData = questData;
+            _isAccepted = isAccepted;
+        }
+    }
+}
