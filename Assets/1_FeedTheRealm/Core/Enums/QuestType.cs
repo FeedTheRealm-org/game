94b5b5e61d2578ead8558c2dccc2e3c28135d272
maxogod <maxi18wars@gmail.com>
2026-01-14 12:49:00 -0300

    feat: add quest type enum with slay/npc interact variants

---
+++ Assets/1_FeedTheRealm/Core/Enums/QuestType.cs
@@ -0,0 +1,14 @@
+using System;
+using UnityEngine;
+
+namespace Game.Core.Enum
+{
+    /// <summary>
+    /// Enum representing different types of quests.
+    /// </summary>
+    public enum QuestType
+    {
+        EnemySlays,
+        NPCInteract,
+    }
+}
