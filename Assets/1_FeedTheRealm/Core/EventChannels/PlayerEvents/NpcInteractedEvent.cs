cedf98c76cd78e2c95e8a21d62afe5368fe3e1b9
maxogod <maxi18wars@gmail.com>
2026-01-14 19:29:10 -0300

    refactor: make npc interact event use the npc interact data

--- Assets/1_FeedTheRealm/Core/EventChannels/PlayerEvents/NpcInteractedEvent.cs
+++ Assets/1_FeedTheRealm/Core/EventChannels/PlayerEvents/NpcInteractedEvent.cs
@@ -1,7 +1,8 @@
+using Game.Core.Interactions;
 using UnityEngine;
 
 namespace Game.Core.Events
 {
     [CreateAssetMenu(menuName = "Events/PlayerEvents/Npc Interacted")]
-    public class NpcInteractedEvent : EventChannelSO { }
+    public class NpcInteractedEvent : EventChannelSO<NpcInteractedData> { }
 }
