360723f2ac439bbcb44ccd451aa1e0ccb85e83a1
Agustín Barbalase <agustinbarbalase@gmail.com>
2026-01-19 23:39:38 -0300

    feat: added ShopMenu into scene and missing events

---
+++ Assets/1_FeedTheRealm/Core/Systems/Interactions/ShopInteractedData.cs
@@ -0,0 +1,10 @@
+namespace Game.Core.Interactions
+{
+    /// <summary>
+    /// Data structure representing player interaction with an Shop.
+    /// </summary>
+    public class ShopInteractedData
+    {
+        public string PlayerId;
+    }
+}
