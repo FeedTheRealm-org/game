b7fe2e309f366e4616decbbcc4c9be8b89dde0cf
maxogod <maxi18wars@gmail.com>
2026-02-08 16:32:46 -0300

    feat: new event channels for network adapter -> service

---
+++ Assets/1_FeedTheRealm/Core/EventChannels/NetworkEvents/ReceivedTransactionCommandEvent.cs
@@ -0,0 +1,10 @@
+using Game.Core.RpcMessages;
+using UnityEngine;
+
+namespace Game.Core.Events
+{
+    // ReceivedTransactionCommandEvent is an event treated as a mpsc channel
+    // Do not subscribe more than one method to it! (Only server-side NetworkService)
+    [CreateAssetMenu(menuName = "Events/NetworkEvents/Received Transaction Command")]
+    public class ReceivedTransactionCommandEvent : EventChannelSO<TransactionCommandDTO> { }
+}
