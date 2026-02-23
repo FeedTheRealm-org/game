4600c0e678b9da70c37c38bb5b31d9bc3b3c56fe
maxogod <maxi18wars@gmail.com>
2026-02-23 18:38:50 -0300

    feat: new ticks event channels

---
+++ Assets/1_FeedTheRealm/Core/Client/EventChannels/Ticks/TickEvent.cs
@@ -0,0 +1,8 @@
+using FTR.Core.Common.EventChannels;
+using UnityEngine;
+
+namespace FTR.Core.Client.EventChannels.Ticks
+{
+    [CreateAssetMenu(menuName = "Events/Ticks/Tick")]
+    public class TickEvent : EventChannelSO { }
+}
