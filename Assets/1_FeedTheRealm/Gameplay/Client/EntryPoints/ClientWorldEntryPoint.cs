9319e7fbdddabcb8c372eee86b15f50736664cf2
sev <santisevitz@hotmail.com>
2026-02-27 19:06:41 -0300

    fix: fix RC in client world entrypoint

--- Assets/1_FeedTheRealm/Gameplay/Client/EntryPoints/ClientWorldEntryPoint.cs
+++ Assets/1_FeedTheRealm/Gameplay/Client/EntryPoints/ClientWorldEntryPoint.cs
@@ -10,6 +10,8 @@ public class ClientWorldEntryPoint : IStartable, ITickable, IFixedTickable, ILat
 
     private LateTickEvent lateTickEvent;
 
+    private bool isInitialized = false;
+
     public void ClientWorldEntrypoint(
         TickEvent tickEvent,
         FixedTickEvent fixedTickEvent,
@@ -19,6 +21,7 @@ public class ClientWorldEntryPoint : IStartable, ITickable, IFixedTickable, ILat
         this.tickEvent = tickEvent;
         this.fixedTickEvent = fixedTickEvent;
         this.lateTickEvent = lateTickEvent;
+        isInitialized = true;
     }
 
     public void Start()
@@ -28,16 +31,22 @@ public class ClientWorldEntryPoint : IStartable, ITickable, IFixedTickable, ILat
 
     public void Tick()
     {
+        if (!isInitialized)
+            return;
         tickEvent.Raise();
     }
 
     public void FixedTick()
     {
+        if (!isInitialized)
+            return;
         fixedTickEvent.Raise();
     }
 
     public void LateTick()
     {
+        if (!isInitialized)
+            return;
         lateTickEvent.Raise();
     }
 }
