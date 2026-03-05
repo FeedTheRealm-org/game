f55bb26b3bb304b5ae118d8acb3425faa4f4cfe7
maxogod <maxi18wars@gmail.com>
2026-03-05 17:17:37 -0300

    feat: move server systems and add use system

---
+++ Assets/1_FeedTheRealm/Gameplay/Server/Characters/Systems/UseSystem.cs
@@ -0,0 +1,12 @@
+using FTR.Core.Common.Config;
+using FTR.Core.Common.Utils;
+using FTR.Core.Server.Config;
+using FTR.Core.Server.EventChannels;
+using FTR.Gameplay.Common.NetworkEntities.Characters;
+using UnityEngine;
+using VContainer;
+
+namespace FTR.Gameplay.Server.Characters
+{
+    public class UseSystem : MonoBehaviour, IGameTickable { }
+}
