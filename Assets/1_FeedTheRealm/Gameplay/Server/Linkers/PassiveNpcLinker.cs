7708ea589d9f382fd5223d21239490c856b95a47
maxogod <maxi18wars@gmail.com>
2026-03-10 20:23:09 -0300

    feat: add base character linker

--- Assets/1_FeedTheRealm/Gameplay/Server/Linkers/PassiveNpcLinker.cs
+++ Assets/1_FeedTheRealm/Gameplay/Server/Linkers/PassiveNpcLinker.cs
@@ -1,9 +1,20 @@
-using FTR.Gameplay.Common.Linkers;
+using FTR.Core.Server;
 using UnityEngine;
+using VContainer;
 
 namespace FTR.Gameplay.Server.Linkers;
 
-public class ServerPassiveNpcLinker : PassiveNpcLinker
+public class ServerPassiveNpcLinker : BaseServerPlayerLinker
 {
-    public override void Link(GameObject gameObject) { }
+    public ServerPassiveNpcLinker(
+        WorldMonitor world,
+        ServerPrefabProvider prefabProvider,
+        IObjectResolver resolver
+    )
+        : base(world, prefabProvider, resolver) { }
+
+    public override void Link(GameObject gameObject)
+    {
+        base.Link(gameObject);
+    }
 }
