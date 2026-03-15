40a9e4c1595452f28f79bba3bdce21b50c8e7dc2
Agustín Barbalase <agustinbarbalase@gmail.com>
2026-03-14 21:00:15 -0300

    feat: created FastSlotStateStorage

---
+++ Assets/1_FeedTheRealm/Gameplay/Common/NetworkEntities/Characters/FastSlotStateStorage.cs
@@ -0,0 +1,58 @@
+using System;
+using Mirror;
+
+namespace FTR.Gameplay.Common.NetworkEntities.LootItem
+{
+    public class FastSlotStateStorage : NetworkBehaviour
+    {
+        [SyncVar(hook = nameof(OnLastItemSync))]
+        private LastItemData lastItemData;
+
+        [SyncVar(hook = nameof(OnLastSwappedItemSync))]
+        private LastSwappedItemData lastSwappedItemData;
+
+        [SyncVar(hook = nameof(OnLastDroppedItemSync))]
+        private LastItemData lastDroppedItemData;
+
+        public LastItemData LastItem => lastItemData;
+        public LastSwappedItemData LastSwappedItem => lastSwappedItemData;
+        public LastItemData LastDroppedItem => lastDroppedItemData;
+
+        public event Action<LastItemData> OnLastItemChanged;
+        public event Action<LastSwappedItemData> OnLastSwappedItemChanged;
+        public event Action<LastItemData> OnLastDroppedItemChanged;
+
+        [Server]
+        public void AddItem(string itemId, int position)
+        {
+            lastItemData = new LastItemData(itemId, position);
+        }
+
+        [Server]
+        public void SwapItems(int sourcePosition, int targetPosition)
+        {
+            lastSwappedItemData = new LastSwappedItemData(sourcePosition, targetPosition);
+        }
+
+        [Server]
+        public void DropItem(int position)
+        {
+            lastDroppedItemData = new LastItemData(string.Empty, position);
+        }
+
+        private void OnLastItemSync(LastItemData oldData, LastItemData newData)
+        {
+            OnLastItemChanged?.Invoke(newData);
+        }
+
+        private void OnLastSwappedItemSync(LastSwappedItemData oldData, LastSwappedItemData newData)
+        {
+            OnLastSwappedItemChanged?.Invoke(newData);
+        }
+
+        private void OnLastDroppedItemSync(LastItemData oldData, LastItemData newData)
+        {
+            OnLastDroppedItemChanged?.Invoke(newData);
+        }
+    }
+}
