4242abc9df04b8e2886b25d673e4cd1bad6e96c2
maxogod <maxi18wars@gmail.com>
2026-03-19 19:33:30 -0300

    feat: add tcp healthcheck server

---
+++ Assets/1_FeedTheRealm/Core/Server/Healthcheck/HealthcheckServer.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FTR.Core.Server.Healthcheck;
+
+public class HealthcheckServer
+{
+    private readonly ushort _port;
+    private TcpListener _listener;
+    private CancellationTokenSource _cts;
+    private Task _listenerTask;
+
+    public HealthcheckServer(FTR.Core.Common.Config.Config config)
+    {
+        _port = config.HealthcheckPort;
+    }
+
+    public void Start()
+    {
+        _cts = new CancellationTokenSource();
+        _listener = new TcpListener(IPAddress.Any, _port);
+        _listener.Start();
+
+        _listenerTask = Task.Run(async () =>
+        {
+            while (!_cts.Token.IsCancellationRequested)
+            {
+                try
+                {
+                    var client = await _listener.AcceptTcpClientAsync();
+                    client.Close();
+                }
+                catch (SocketException)
+                {
+                    break;
+                }
+                catch (ObjectDisposedException)
+                {
+                    break;
+                }
+            }
+        });
+    }
+
+    public async Task CloseAsync()
+    {
+        _cts?.Cancel();
+        _listener?.Stop();
+
+        if (_listenerTask != null)
+            await _listenerTask;
+    }
+}
