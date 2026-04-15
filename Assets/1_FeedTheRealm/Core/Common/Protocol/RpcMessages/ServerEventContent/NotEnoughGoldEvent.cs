f51286b2e044a0f146c8fcd6d9d9a6278f13e132
Agustín Barbalase <agustinbarbalase@gmail.com>
2026-04-15 15:48:18 -0300

    chore: regenerate protocol messages

--- Assets/1_FeedTheRealm/Core/Common/Protocol/RpcMessages/ServerEventContent/NotEnoughGoldEvent.cs
+++ Assets/1_FeedTheRealm/Core/Common/Protocol/RpcMessages/ServerEventContent/NotEnoughGoldEvent.cs
@@ -38,6 +38,7 @@ namespace FTR.Core.Common.Protocol.RpcMessages {
 
   }
   #region Messages
+  [global::System.Diagnostics.DebuggerDisplayAttribute("{ToString(),nq}")]
   public sealed partial class NotEnoughGoldEventContent : pb::IMessage<NotEnoughGoldEventContent>
   #if !GOOGLE_PROTOBUF_REFSTRUCT_COMPATIBILITY_MODE
       , pb::IBufferMessage
@@ -222,7 +223,11 @@ namespace FTR.Core.Common.Protocol.RpcMessages {
     #else
       uint tag;
       while ((tag = input.ReadTag()) != 0) {
-        switch(tag) {
+      if ((tag & 7) == 4) {
+        // Abort on any end group tag.
+        return;
+      }
+      switch(tag) {
           default:
             _unknownFields = pb::UnknownFieldSet.MergeFieldFrom(_unknownFields, input);
             break;
@@ -245,7 +250,11 @@ namespace FTR.Core.Common.Protocol.RpcMessages {
     void pb::IBufferMessage.InternalMergeFrom(ref pb::ParseContext input) {
       uint tag;
       while ((tag = input.ReadTag()) != 0) {
-        switch(tag) {
+      if ((tag & 7) == 4) {
+        // Abort on any end group tag.
+        return;
+      }
+      switch(tag) {
           default:
             _unknownFields = pb::UnknownFieldSet.MergeFieldFrom(_unknownFields, ref input);
             break;
