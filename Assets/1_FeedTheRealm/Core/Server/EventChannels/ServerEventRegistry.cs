a01955c4488631bd2a305ca4373a15cdad8c028f
niko <nrsanchez@fi.uba.ar>
2026-04-21 21:04:51 -0300

    chores: removed unnecessary events.

--- Assets/1_FeedTheRealm/Core/Server/EventChannels/ServerEventRegistry.cs
+++ Assets/1_FeedTheRealm/Core/Server/EventChannels/ServerEventRegistry.cs
@@ -16,9 +16,6 @@ namespace FTR.Core.Server.EventChannels
         public QuestRewardItemEvent questRewardItemEvent;
         public NpcQuestCompletedEvent npcQuestCompletedEvent;
         public PlayerQuestDecisionEvent playerQuestDecisionEvent;
-        public ItemEquippedEvent itemEquippedEvent;
-        public PlayerBuffSpeedEvent playerBuffSpeedEvent;
-        public PlayerHealEvent playerHealEvent;
 
         public void RegisterAll(IContainerBuilder builder)
         {
@@ -31,9 +28,6 @@ namespace FTR.Core.Server.EventChannels
             builder.RegisterInstance(questRewardItemEvent);
             builder.RegisterInstance(npcQuestCompletedEvent);
             builder.RegisterInstance(playerQuestDecisionEvent);
-            builder.RegisterInstance(itemEquippedEvent);
-            builder.RegisterInstance(playerBuffSpeedEvent);
-            builder.RegisterInstance(playerHealEvent);
         }
 
         private void Validate()
@@ -45,9 +39,6 @@ namespace FTR.Core.Server.EventChannels
             ValidateField(questRewardItemEvent, nameof(questRewardItemEvent));
             ValidateField(npcQuestCompletedEvent, nameof(npcQuestCompletedEvent));
             ValidateField(playerQuestDecisionEvent, nameof(playerQuestDecisionEvent));
-            ValidateField(itemEquippedEvent, nameof(itemEquippedEvent));
-            ValidateField(playerBuffSpeedEvent, nameof(playerBuffSpeedEvent));
-            ValidateField(playerHealEvent, nameof(playerHealEvent));
         }
 
         private void ValidateField(Object field, string fieldName)
