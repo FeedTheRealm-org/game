71af60ed996b1fe1487e2da88aa1fb5e26bb51fd
sev <santisevitz@hotmail.com>
2026-04-25 10:26:25 -0300

    fix: optimized the colliders in game to directly not render textures at all

--- Assets/1_FeedTheRealm/Gameplay/Common/Environment/Structures/Scripts/ColliderRegistry.cs
+++ Assets/1_FeedTheRealm/Gameplay/Common/Environment/Structures/Scripts/ColliderRegistry.cs
@@ -1,14 +1,12 @@
 using Enums;
+using FTR.Core.Common.Config;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "ColliderRegistry", menuName = "Scriptable Objects/Collider Registry")]
 public class ColliderRegistry : ScriptableObject
 {
     [SerializeField]
-    private LayerMask cubeColliderLayerMask;
-
-    [SerializeField]
-    private LayerMask slopeColliderLayerMask;
+    private Config config;
 
     [SerializeField]
     private GameObject cubeColliderPrefab;
@@ -18,17 +16,29 @@ public class ColliderRegistry : ScriptableObject
 
     public (GameObject, int) GetColliderPrefab(ColliderType colliderType)
     {
+        GameObject prefab;
+        int layer;
+
         switch (colliderType)
         {
             case ColliderType.Slope:
-                return (slopeColliderPrefab, (int)Mathf.Log(slopeColliderLayerMask.value, 2));
+                prefab = slopeColliderPrefab;
+                layer = (int)Mathf.Log(config.slopeColliderLayerMask.value, 2);
+                break;
             case ColliderType.Cube:
-                return (cubeColliderPrefab, (int)Mathf.Log(cubeColliderLayerMask.value, 2));
             default:
-                Debug.LogWarning(
-                    $"ColliderRegistry: No prefab found for collider type {colliderType}, defaulting to cube."
-                );
-                return (cubeColliderPrefab, (int)Mathf.Log(cubeColliderLayerMask.value, 2));
+                if (colliderType != ColliderType.Cube)
+                    Debug.LogWarning(
+                        $"ColliderRegistry: No prefab found for collider type {colliderType}, defaulting to cube."
+                    );
+                prefab = cubeColliderPrefab;
+                layer = (int)Mathf.Log(config.cubeColliderLayerMask.value, 2);
+                break;
         }
+        prefab.SetActive(true);
+        foreach (var renderer in prefab.GetComponentsInChildren<Renderer>())
+            renderer.enabled = config.enableColliderView;
+
+        return (prefab, layer);
     }
 }
